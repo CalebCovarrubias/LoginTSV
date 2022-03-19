12e69e1f3fbae1f8e80c501e146de5faa6bf68fe
Jose Caleb Covarrubias Perez <160271@upslp.edu.mx>
2022-03-18 21:22:44 -0600

    Correcion de prueba en el HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace LoginTSV.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Tu contacto de la pagina";
 
             return View();
         }
